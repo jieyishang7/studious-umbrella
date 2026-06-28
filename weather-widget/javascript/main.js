311caabed0cdd0cd0a98a5ca160a54d26735d1ee
jieyishang7 <jieyishang7@gmail.com>
2026-06-27 22:40:50 -0400

    events

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -2,12 +2,29 @@ console.log("Hello World!");
 
 const API_KEY = config.WEATHER_API_KEY;
 
-function getWeatherData() {
-    fetch(
-        `https://api.openweathermap.org/data/2.5/weather?zip=10128,us&appid=${API_KEY}&units=metric`
-    )
-    .then(response => response.json())
-    .then(data => console.log(data))
-    }
+let input = document.querySelector(".zipcode");
+let btn = document.querySelector(".search-button");
 
-getWeatherData()
+function getWeatherData(zipcode) {
+let API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&units=metric&APPID=${API_KEY}`;
+  fetch(API_ENDPOINT)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+
+        document.querySelector(".temperature").textContent = Math.round(data.main.temp) + "°C";
+      document.querySelector(".city_name").textContent = data.name;
+    })
+    .catch((error) => {
+      console.error("error:", error);
+    });
+  }
+
+function getZipCode(e) {
+  e.preventDefault();
+
+  let ZIP_CODE = input.value;
+  getWeatherData(ZIP_CODE);
+}
+
+btn.addEventListener("click", getZipCode);
